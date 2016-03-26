ef7ca782f6fafa20ef53d5f10f9e8b290276f20a
Nick <nick.van.eijk@itility.nl>
2016-03-26 13:02:29 +0100

    Fixed invalid cron

--- warreminder.js
+++ warreminder.js
@@ -1,13 +1,16 @@
 var cron = require('cron').CronJob
-var cronTimes = ["0 0 21 * 5", "0 0 21 * 1"]
+var cronTimes = ["0 0 21 * * 5", "0 0 21 * * 1"]
 
 var startReminders = (rtm) => {
+  console.log('start reminders')
   cronTimes.forEach( (cronTime) => {
+    rtm.sendMessage(`Debug: cron ${cronTime} starting`, 'C0M8CP4JZ')
     new cron({cronTime: cronTime, onTick: () => { remindMe(rtm) }, start: true});
   })
 }
 
 var remindMe = (rtm) => {
+  console.log('Reminder called')
   rtm.sendMessage('Have you guys started a war yet?', 'C0M8CP4JZ');
 }
 module.exports = startReminders
